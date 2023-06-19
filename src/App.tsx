dc1798cdcd2bc43d3d95f7e7a9187279d9aeb7ee
ArmyofDeux <103246856+Emmanuetoks@users.noreply.github.com>
2023-06-19 08:46:27 -0700

    component: Make header section responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar";
+import Illustration from "./assets/illustration/illustration-hero.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <header className="header">
+        <Navbar />
 
+        <div className="header__intro px-9 pt-16 md:pl-24 md:pr-12 flex flex-col-reverse md:flex-row">
+
+          <div className="header__intro-left basis-2/5 space-y-6 text-center md:text-left">
+            <h1 className="capitalize font-semibold text-4xl text-blue-200">a simple bookmark manager</h1>
+            <p className="text-blue-100">
+              A clean and simple interface to organize your favourite websites.
+              Open a new browser tab and see your sites load instantly. Try it
+              for free.
+            </p>
+
+            <div className="header__intro-download-buttons">
+              <a>
+                <button className="px-4 py-3 bg-blue-100 text-white rounded">
+                  Get it on Chrome
+                </button>
+              </a>
+
+              <a>
+                <button className="px-4 py-3 text-blue-200 font-medium">Get it on Firefox</button>
+              </a>
+            </div>
+          </div>
+
+          <div className="header__intro-right basis-3/5">
+            <div className="max-w-sm mx-auto">
+              <img className="" src={Illustration} />
+            </div>
+          </div>
+        </div>
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+// const defaultTheme = require('tailwindcss/defaultTheme')
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
-  content: [],
+  content: ["./src/**/*.{tsx,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        blue: {
+          100: "hsl(229, 8%, 60%)",
+          200: "hsl(229, 31%, 21%)",
+        },
+        white: "#ffff",
+      },
+      backgroundColor: {
+        red: "hsl(0, 94%, 66%)",
+        blue: {
+          100: "hsl(231, 69%, 60%)",
+          200: "hsl(229, 8%, 60%)",
+        },
+      },
+      fontFamily: {
+        sans: ["Rubik", defaultTheme],
+      },
+    },
   },
   plugins: [],
-}
-
+};
