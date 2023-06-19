b13f56dcecfbc26d09b52a5e36cbe38209a4e9ef
ArmyofDeux <103246856+Emmanuetoks@users.noreply.github.com>
2023-06-19 16:17:38 -0700

    Finish NavBar Mobile Menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,19 +10,31 @@ export default {
         blue: {
           100: "hsl(229, 8%, 60%)",
           200: "hsl(229, 31%, 21%)",
+          300: "hsl(231, 69%, 60%)",
         },
         white: "#ffff",
       },
       backgroundColor: {
         red: "hsl(0, 94%, 66%)",
         blue: {
-          100: "hsl(231, 69%, 60%)",
-          200: "hsl(229, 8%, 60%)",
+          100: "hsl(229, 8%, 60%)",
+          200: "hsl(231, 69%, 60%)",
         },
+        "mobile-menu": "hsla(229, 31%, 21%, 0.952)  ",
       },
       fontFamily: {
         sans: ["Rubik", defaultTheme],
       },
+
+      borderColor: {
+        blue: {
+          100: "hsl(229, 8%, 60%)",
+          200: "hsl(231, 69%, 60%)",
+        },
+      },
+      // translate: {
+      //   '0.2': '20%'
+      // }
     },
   },
   plugins: [],
